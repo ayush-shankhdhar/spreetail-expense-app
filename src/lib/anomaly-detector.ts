7d898121f3ca881dba5a4cad8d1e62efbe56d87d
Ayush Shankhdhar <ayushshankhdhar44@gmail.com>
2026-06-14 05:21:34 +0530

    create anomaly detection framework

---
+++ src/lib/anomaly-detector.ts
@@ -0,0 +1,35 @@
+export function detectDuplicate() {
+  return [];
+}
+
+export function detectMissingCurrency() {
+  return [];
+}
+
+export function detectNegativeAmount() {
+  return [];
+}
+
+export function detectZeroAmount() {
+  return [];
+}
+
+export function detectNameVariations() {
+  return [];
+}
+
+export function detectInvalidMembers() {
+  return [];
+}
+
+export function detectAmbiguousDates() {
+  return [];
+}
+
+export function detectSettlementExpense() {
+  return [];
+}
+
+export function detectSplitTypeMismatch() {
+  return [];
+}
